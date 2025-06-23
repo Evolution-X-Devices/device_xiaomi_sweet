5026067fbc1394c52ce3a74989248625ebd96377
therealmharc <therealmharc@gmail.com>
2025-06-23 11:52:35 +0000

    sweet: initialize Evolution-X 10.x
    - enable Broken Build Rules
    - set boot and vendor spl to platform
    - setup dependencies
    
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -9,20 +9,49 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from sweet device
+# Inherit from sweet device.
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common LineageOS / Evolution-X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Bootanimation
+TARGET_INCLUDE_BOOT_ANIMATIONS := true
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2400
+
+# Targets
+TARGET_INCLUDE_ACCORD := false
+TARGET_HAS_UDFPS := false
+TARGET_INCLUDE_VIPERFX := false
+TARGET_DISABLE_LINEAGE_SDK := false
+TARGET_DISABLE_EPPE := false
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
+# BCR
+BUILD_BCR := true
+
+# Bypass charge
+BYPASS_CHARGE_SUPPORTED := true
+
+# Device identifier
 PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
+# GMS
+ifeq ($(WITH_GMS),true)
+TARGET_USES_MINI_GAPPS := false
+TARGET_USES_PICO_GAPPS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+endif
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Properties
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys" \
     BuildFingerprint=Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
